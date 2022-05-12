b0074894664cbe8ec320f1f8fdf40fcc466ff8a5
Keisuke Miyata <milion1467@gmail.com>
2022-05-12 17:37:23 +0900

    feat: Material Icon導入

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,15 +1,20 @@
 <template>
   <div class="main">
     <p>{{ text }}</p>
+    <mdiHandBackRightOutline size="48" />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import mdiHandBackRightOutline from "vue-material-design-icons/HandBackRightOutline.vue";
 
 export default defineComponent({
   name: "Content",
   props: {},
+  components: {
+    mdiHandBackRightOutline,
+  },
   setup() {
     const text = "Hi, there.";
 
